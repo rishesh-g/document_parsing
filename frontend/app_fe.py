99b5a175a9aa62e044bcc731411150a758f76e70
risheshg <risheshg@gmail.com>
2025-06-25 09:39:21 +0000

    Frontend Containerized

--- frontend/app_fe.py
+++ frontend/app_fe.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import requests
 import config
+import os
 
 st.title("Upload an Image File")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+APP_URL = os.getenv("APP_URL", "http://localhost:5000")
+
 
 if uploaded_file is not None:
     # Prepare the file for POST request
@@ -12,7 +16,7 @@ if uploaded_file is not None:
         'file': (uploaded_file.name, uploaded_file, uploaded_file.type)
     }
     # Send the file to the Flask backend
-    response = requests.post(config.APP_URL, files=files)
+    response = requests.post(f'{APP_URL}/upload', files=files)
     if response.ok:
         st.write("Result from backend:")
         st.json(response.json())
